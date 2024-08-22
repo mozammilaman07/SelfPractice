94d9cdfed4914d34a3db4959643a46d6a349c678
Md Mozammil Aman <amanmozammil.07@gmail.com>
2024-08-22 13:45:33 +0530

    bubble sort

---
+++ sorting.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+int main()
+{
+
+    int arr[] = {5, 4, 3, 2, 1};
+    int n = 5;
+    cout << "Before sorting" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    bubbleSort(arr, n);
+    cout << endl
+         << "After sorting" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
