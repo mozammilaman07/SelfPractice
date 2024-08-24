d83edff18b2d0e64a9a27bb3317dd1b2437c13b1
Md Mozammil Aman <amanmozammil.07@gmail.com>
2024-08-24 18:15:17 +0530

    made a function for array print

--- sorting.cpp
+++ sorting.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 using namespace std;
 
+void printArray(int arr[], int n)
+{
+    cout << "print the array " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
 int main()
 {
     int arr[] = {40, 50, 70, 1, 24, 6, 45, 30};
     int n = 8;
+
+    printArray(arr, n);
 }
