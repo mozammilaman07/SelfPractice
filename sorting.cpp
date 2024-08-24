d91c47a72bb76f0a5ad42bd06fe742248af5ce83
Md Mozammil Aman <amanmozammil.07@gmail.com>
2024-08-24 18:11:54 +0530

    practice the sorting algo

--- sorting.cpp
+++ sorting.cpp
@@ -1,35 +1,8 @@
 #include <iostream>
 using namespace std;
 
-void bubbleSort(int arr[], int n)
-{
-    for (int i = 0; i < n - 1; i++)
-    {
-        for (int j = 0; j < n - i - 1; j++)
-        {
-            if (arr[j] > arr[j + 1])
-            {
-                swap(arr[j], arr[j + 1]);
-            }
-        }
-    }
-}
-
 int main()
 {
-
-    int arr[] = {5, 4, 8, 5, 2, 14, 7, 6, 40, 80, 4, 3, 2, 1};
-    int n = 14;
-    cout << "Before sorting" << endl;
-    for (int i = 0; i < n; i++)
-    {
-        cout << arr[i] << " ";
-    }
-    bubbleSort(arr, n);
-    cout << endl
-         << "After sorting" << endl;
-    for (int i = 0; i < n; i++)
-    {
-        cout << arr[i] << " ";
-    }
+    int arr[] = {40, 50, 70, 1, 24, 6, 45, 30};
+    int n = 8;
 }
