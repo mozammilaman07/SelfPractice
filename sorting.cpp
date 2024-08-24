32164ea41b1c6b88829f4db2f41f4b0b56871881
Md Mozammil Aman <amanmozammil.07@gmail.com>
2024-08-24 18:18:21 +0530

    sorted array by bubblesort

--- sorting.cpp
+++ sorting.cpp
@@ -10,10 +10,27 @@ void printArray(int arr[], int n)
     }
     cout << endl;
 }
+
+void bubbleSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
 int main()
 {
     int arr[] = {40, 50, 70, 1, 24, 6, 45, 30};
     int n = 8;
 
+    printArray(arr, n);
+
+    bubbleSort(arr, n);
     printArray(arr, n);
 }
