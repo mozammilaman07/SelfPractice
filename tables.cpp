58cbcfe25b752a35e0d0e79d7361eac8013c5122
Md Mozammil Aman <amanmozammil.07@gmail.com>
2024-08-25 21:41:41 +0530

    tables print added

---
+++ tables.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 6; i < n; i++)
+    {
+        cout << "Table of " << i << endl;
+        for (int j = 1; j <= 10; j++)
+        {
+            int value = i * j;
+            cout << i << " * " << j << " = " << value << endl;
+        }
+        cout << endl;
+    }
+}
