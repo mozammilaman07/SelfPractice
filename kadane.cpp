2d560bc7d8e22daddd54556e63f50dd8a2166b7c
Md Mozammil Aman <amanmozammil.07@gmail.com>
2024-09-01 23:26:23 +0530

    created a function

--- kadane.cpp
+++ kadane.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 using namespace std;
 
+int maxSubArray(int arr[], int n)
+{
+}
+
 int main()
 {
     int arr[] = {5, 4, -1, 7, 8};
     int n = 5;
+
+    int kadane = maxSubArray(arr, n);
 }
