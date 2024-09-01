2edc111b5fef2969e5f87c7e8f99cb1689c3fda6
Md Mozammil Aman <amanmozammil.07@gmail.com>
2024-09-01 23:24:28 +0530

    removed previous method

--- kadane.cpp
+++ kadane.cpp
@@ -5,13 +5,4 @@ int main()
 {
     int arr[] = {5, 4, -1, 7, 8};
     int n = 5;
-
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = i; j < n; j++)
-        {
-            cout << "sub Arrays are : " << arr[j] << "," << endl;
-        }
-        cout << endl;
-    }
 }
