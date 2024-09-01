34c95646c92a07155a6f958a9d662d1eb4c34b3d
Md Mozammil Aman <amanmozammil.07@gmail.com>
2024-09-01 23:31:08 +0530

    added kadane algo

--- kadane.cpp
+++ kadane.cpp
@@ -3,6 +3,18 @@ using namespace std;
 
 int maxSubArray(int arr[], int n)
 {
+    int sum = 0;
+    int maxi = arr[0];
+    for (int i = 0; i < n; i++)
+    {
+        sum = sum + arr[i];
+        maxi = max(maxi, sum);
+        if (sum < 0)
+        {
+            sum = 0;
+        }
+    }
+    return maxi;
 }
 
 int main()
@@ -11,4 +23,5 @@ int main()
     int n = 5;
 
     int kadane = maxSubArray(arr, n);
+    cout << "print " << kadane << endl;
 }
