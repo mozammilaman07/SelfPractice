c0ee9c79c1996f8ab8b3f872b7843d2cc02c7a96
Md Mozammil Aman <amanmozammil.07@gmail.com>
2024-09-01 23:07:46 +0530

    tried by myself to print subarrays

--- kadane.cpp
+++ kadane.cpp
@@ -4,4 +4,14 @@ using namespace std;
 int main()
 {
     int arr[] = {5, 4, -1, 7, 8};
+    int n = 5;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i; j < n; j++)
+        {
+            cout << "sub Arrays are : " << arr[j] << "," << endl;
+        }
+        cout << endl;
+    }
 }
