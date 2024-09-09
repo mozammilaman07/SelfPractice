44864bccea1a84682e1bf475d532583b23580cc1
Md Mozammil Aman <amanmozammil.07@gmail.com>
2024-09-09 23:12:02 +0530

    created largest func

--- 2ndLargest.cpp
+++ 2ndLargest.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 using namespace std;
 
+int largest(int arr[], int n)
+{
+    int largest = 0;
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] > arr[largest])
+        {
+            largest = i;
+        }
+        }
+    return largest;
+}
+
 int main()
 {
     int arr[] = {10, 54, 21, 6, 41, 14};
