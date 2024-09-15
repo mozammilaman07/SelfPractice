70ca69009e4f8d0dbb30335bf10d05e9933b7f9c
Md Mozammil Aman <amanmozammil.07@gmail.com>
2024-09-15 22:42:38 +0530

    declared func

--- kadanes.cpp
+++ kadanes.cpp
@@ -3,6 +3,18 @@ using namespace std;
 
 void kadanes(int arr[], int n)
 {
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i; j < n; j++)
+        {
+            for (int k = i; k <= j; k++)
+            {
+                cout << arr[i];
+            }
+            cout << " ";
+        }
+        cout << endl;
+    }
 }
 
 int main()
