2d05a15ac28f87b876b24b93c135774e871fd961
Md Mozammil Aman <amanmozammil.07@gmail.com>
2024-09-20 12:47:03 +0530

    fixed header file

--- merge.cpp
+++ merge.cpp
@@ -1,4 +1,4 @@
-#include <iosrtream>
+#include <iostream>
 using namespace std;
 
 int main()
@@ -6,4 +6,6 @@ int main()
     int arr1[5] = {1, 5, 6, 7, 9};
     int arr2[3] = {2, 4, 8};
     int arr3[8] = {0};
+
+    merge(arr1, 5, arr2, 3, arr3);
 }
