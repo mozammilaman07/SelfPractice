63eac313a4ba6acc33505a010f746774901c963c
Md Mozammil Aman <amanmozammil.07@gmail.com>
2024-10-09 12:36:40 +0530

    code done

--- ques.cpp
+++ ques.cpp
@@ -3,6 +3,14 @@ using namespace std;
 
 void pairs(int arr[], int n)
 {
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << arr[i] << "," << arr[j] << endl;
+        }
+        cout << endl;
+    }
 }
 
 int main()
