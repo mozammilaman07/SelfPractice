b617f11e659298c37743acdee1412efa6eab8260
Md Mozammil Aman <amanmozammil.07@gmail.com>
2024-10-12 22:51:00 +0530

    debug code

--- 2dArray.cpp
+++ 2dArray.cpp
@@ -3,10 +3,19 @@ using namespace std;
 
 int main()
 {
-    int arr[2][3] = {
+    int arr[2][4] = {
         {10, 20, 30, 40},
         {11, 12, 13, 14}};
 
     int row = 2;
     int col = 4;
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            cout << arr[i][j];
+        }
+        cout << endl;
+    }
 }
