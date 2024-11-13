fcd2ec1756337be88b7118f4937a10cb1922b73f
Md Mozammil Aman <amanmozammil.07@gmail.com>
2024-11-13 23:48:07 +0530

    rotate array func

--- selfPractice.cpp
+++ selfPractice.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
 using namespace std;
 
+void rotate(int arr[], int n, int k)
+{
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] % k ==
+    }
+}
 int main()
 {
     int arr[] = {10, 20, 30, 40, 50, 60, 70};
     int n = 7;
+    int k = 2;
+
+    rotate(arr, n);
 }
