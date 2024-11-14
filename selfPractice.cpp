57f66018ef5562f7d4df9bcf45cc744631ad8091
Md Mozammil Aman <amanmozammil.07@gmail.com>
2024-11-14 22:29:17 +0530

    sort negative and positive

---
+++ selfPractice.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+void moveNeg(int arr[], int n)
+{
+    int j = 0;
+    for (int i = 0; i < n; i++)
+    {
+
+        if (arr[i] < 0)
+        {
+            swap(arr[i], arr[j]);
+            j++;
+        }
+    }
+}
+
+int main()
+{
+
+    int arr[] = {23, -5, 54, -64, -12, 60, 11};
+    int n = 7;
+    cout << "Before" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    moveNeg(arr, n);
+    cout << "After" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
