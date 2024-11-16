7520896ba98b97b967711d4ae559fa2c4badec63
Md Mozammil Aman <amanmozammil.07@gmail.com>
2024-11-16 23:46:29 +0530

    code done

--- firstRepeatingElement.cpp
+++ firstRepeatingElement.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 using namespace std;
 
+void firstRepeat(int arr[], int n)
+{
+    bool isRepeat = false;
+    int index = -1;
+    for (int i = 0; i < n; i++)
+    {
+
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[i] == arr[j])
+            {
+                isRepeat = true;
+                index = i;
+            }
+        }
+    }
+
+    cout << "Repeated or not : " << isRepeat;
+    // cout << "at index : " << index;
+}
+
 int main()
 {
     int arr[] = {1, 5, 6, 5, 4, 2, 6, 8};
