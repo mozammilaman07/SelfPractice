5243cb1f114981f7d955807fd3e6cdfb240f30b1
Md Mozammil Aman <amanmozammil.07@gmail.com>
2025-02-15 21:05:19 +0530

    practiced sum of array and find target

--- selfPractice.cpp
+++ selfPractice.cpp
@@ -7,6 +7,29 @@ void printArray(int arr[], int n)
     {
         cout << arr[i] << " ";
     }
+    cout << endl;
+}
+
+int findTarget(int arr[], int n, int target)
+{
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == target)
+        {
+            return i;
+        }
+    }
+    return false;
+}
+int sumArray(int arr[], int n)
+{
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        sum = sum + arr[i];
+    }
+    return sum;
 }
 
 int main()
@@ -14,8 +37,19 @@ int main()
 
     int arr[] = {10, 20, 30, 40, 50, 60, 70};
     int n = 7;
-
+    int target = 50;
+    // int ans = findTarget(arr, n, target);
     printArray(arr, n);
+    int ans = sumArray(arr, n);
+    cout << "sum of array are: " << ans << endl;
 
+    // if (findTarget == false)
+    // {
+    //     cout << "target not found" << endl;
+    // }
+    // else
+    // {
+    //     cout << "target found at index : " << ans << endl;
+    // }
     return 0;
 }
