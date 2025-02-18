39d15b145e3c6c097cf08b303445bbb5dedad117
Md Mozammil Aman <amanmozammil.07@gmail.com>
2025-02-18 10:24:39 +0530

    find minimum number

--- selfPractice.cpp
+++ selfPractice.cpp
@@ -1,55 +1,25 @@
 #include <iostream>
+#include <limits.h>
 using namespace std;
 
-void printArray(int arr[], int n)
+int findMin(int arr[], int n)
 {
+    int minAns = INT_MAX;
     for (int i = 0; i < n; i++)
     {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
-}
-
-int findTarget(int arr[], int n, int target)
-{
-
-    for (int i = 0; i < n; i++)
-    {
-        if (arr[i] == target)
+        if (arr[i] < minAns)
         {
-            return i;
+            minAns = arr[i];
         }
     }
-    return false;
-}
-int sumArray(int arr[], int n)
-{
-    int sum = 0;
-    for (int i = 0; i < n; i++)
-    {
-        sum = sum + arr[i];
-    }
-    return sum;
+    return minAns;
 }
 
 int main()
 {
+    int arr[] = {7, 18, 64, 20, 4, 02, 98, 15};
+    int n = 8;
 
-    int arr[] = {10, 20, 30, 40, 50, 60, 70};
-    int n = 7;
-    int target = 50;
-    // int ans = findTarget(arr, n, target);
-    printArray(arr, n);
-    int ans = sumArray(arr, n);
-    cout << "sum of array are: " << ans << endl;
-
-    // if (findTarget == false)
-    // {
-    //     cout << "target not found" << endl;
-    // }
-    // else
-    // {
-    //     cout << "target found at index : " << ans << endl;
-    // }
-    return 0;
+    int ans = findMin(arr, n);
+    cout << "minimum number in array is : " << ans << endl;
 }
