91a38e2298c382611e8036a904b80614b629a23c
Md Mozammil Aman <amanmozammil.07@gmail.com>
2025-02-24 22:50:49 +0530

    pair array

--- selfPractice.cpp
+++ selfPractice.cpp
@@ -1,34 +1,21 @@
 #include <iostream>
 using namespace std;
 
-void reverseArray(int arr[], int n)
-{
-    int left = 0;
-    int right = n - 1;
-    while (left <= right)
-    {
-        swap(arr[left], arr[right]);
-        left++;
-        right--;
-    }
-}
-
-void printArray(int arr[], int n)
+void pairArray(int arr[], int n)
 {
     for (int i = 0; i < n; i++)
     {
-        cout << arr[i] << " ";
+        for (int j = 0; j < n; j++)
+        {
+            cout << arr[i] << "," << arr[j] << endl;
+        }
     }
-    cout << endl;
 }
 
 int main()
 {
+    int arr[] = {10, 20, 30};
+    int n = 3;
 
-    int arr[] = {10, 20, 30, 40, 50, 60};
-    int n = 6;
-
-    printArray(arr, n);
-    reverseArray(arr, n);
-    printArray(arr, n);
+    pairArray(arr, n);
 }
