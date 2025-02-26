a45faf668b3794b7d296647f3760c72981b779a7
Md Mozammil Aman <amanmozammil.07@gmail.com>
2025-02-26 22:32:49 +0530

    print changed

--- selfPractice.cpp
+++ selfPractice.cpp
@@ -5,7 +5,7 @@ void printArray(int arr[4][3], int row, int col)
 {
     for (int i = 0; i < row; i++)
     {
-        for (int j = 0; j < col; j++)
+        for (int j = i; j < col; j++)
         {
             cout << arr[i][j] << ", ";
         }
