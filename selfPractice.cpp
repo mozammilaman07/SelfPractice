7fe6e166eaafb321bd84456f57448afaeae46ad5
Md Mozammil Aman <amanmozammil.07@gmail.com>
2025-02-28 23:41:39 +0530

    commented out prev code

--- selfPractice.cpp
+++ selfPractice.cpp
@@ -1,24 +1,24 @@
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
-int main()
-{
+// int main()
+// {
 
-    int arr[] = {1, 5, 6, 1, 7, 5};
-    int n = 6;
+//     int arr[] = {1, 5, 6, 1, 7, 5};
+//     int n = 6;
 
-    // precompute
-    int hash[13] = {0};
-    for (int i = 0; i < n; i++)
-    {
-        hash[arr[i]] += 1;
-    }
-    int q;
-    cin >> q;
-    while (q--)
-    {
-        int number;
-        cin >> number;
-        cout << hash[number] << endl;
-    }
-}
+//     // precompute
+//     int hash[13] = {0};
+//     for (int i = 0; i < n; i++)
+//     {
+//         hash[arr[i]] += 1;
+//     }
+//     int q;
+//     cin >> q;
+//     while (q--)
+//     {
+//         int number;
+//         cin >> number;
+//         cout << hash[number] << endl;
+//     }
+// }
