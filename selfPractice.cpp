c7a22d0d97919060e175beb37852729e25a25d4a
Md Mozammil Aman <amanmozammil.07@gmail.com>
2025-03-01 23:57:57 +0530

    binary search code

--- selfPractice.cpp
+++ selfPractice.cpp
@@ -1,11 +1,46 @@
 #include <iostream>
 using namespace std;
 
+int binarySearch(int arr[], int size, int target)
+{
+    int start = 0;
+    int end = size - 1;
+    int mid = (start + end) / 2;
+
+    while (start <= end)
+    {
+        if (arr[mid] == target)
+        {
+            return mid;
+        }
+        if (arr[mid] > target)
+        {
+            end = mid - 1;
+        }
+        if (arr[mid] < target)
+        {
+            start = mid + 1;
+        }
+        mid = (start + end) / 2;
+    }
+    return -1;
+}
+
 int main()
 {
     int arr[] = {10, 20, 30, 40, 50, 60, 70, 80};
     int size = 8;
     int target = 70;
+
+    int ansIndex = binarySearch(arr, size, target);
+    if (ansIndex == -1)
+    {
+        cout << "Target not found" << endl;
+    }
+    else
+    {
+        cout << "target found at index : " << ansIndex << endl;
+    }
 }
 
 // #include <iostream>
